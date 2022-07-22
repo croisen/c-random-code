2e64a424e5d34eeb4485217be0ad65287a8478b1
croisen <absolutenico32@gmail.com>
2022-07-22 17:07:19 +0800

    Solved problem 5, problem 6 being started now

--- project_euler.c
+++ project_euler.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int problem_001() {
@@ -38,27 +39,35 @@ int problem_002() {
 int art_thou_prime(int num_1) {
 	double square_root = sqrt(num_1);
 	int i;
-	int test = 0;
+	int test_pass = 0;
 
-	for (i = 2; i <= 1000; i++) {
-		if (num_1 == i){
-			test = 0;
-			break;
-		} else if (num_1 % i == 0) {
-			test = 1;
+	for (i = 2; i <= num_1; i++) {
+		if (num_1 % i == 0 && num_1 != i) {
+			test_pass = 1;
 			break;
 		}
 	}
 
-	if (test == 1) {
+	if (test_pass == 1) {
 		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int problem_003() {
+	// It can solve the highest prime of the smol number but it takes too long for the big number
+
+	long big_number = 600851475143;
+	int smol_number = 13195, highest_prime = 0, i;
+
+	for (i = 2; i <= smol_number; i++){
+		if (art_thou_prime(i) == 0 && smol_number % i == 0) {
+			highest_prime = i;
+		}
 	}
 
-	if (remainder(square_root, 1.0000) == 0) {
-		return 0;
-	} else {
-		return 1;
-	}
+	return highest_prime;
 }
 
 int reverse_an_integer(int num_1) {
@@ -99,6 +108,33 @@ int problem_004() {
 	return highest_palindrome;
 }
 
+int problem_005() {
+	// Takes a long time
+
+	int i, search;
+	int divisors = 20;
+	int pass_rate = 0;
+	int x = 1;
+
+	for (search = 200000000; search <= 250000000; search++) {
+		pass_rate = 0;
+
+		for (i = 1; i <= divisors; i++) {
+			printf("\rCurrently trying: %09d, Pass rate: %02d", search, pass_rate);
+
+			if (search % i == 0) {
+				pass_rate++;
+			} else {
+				break;
+			}
+		}
+
+		if (pass_rate == divisors) {
+			return search;
+		}
+	}
+}
+
 int main( int argc, char *argv[] ) {
 	if (argc == 2) {
 		if ( 0 == strcmp(argv[1], "problem_001")) {
@@ -124,12 +160,25 @@ int main( int argc, char *argv[] ) {
 			return 0;
 		}
 
+		if ( 0 == strcmp(argv[1], "problem_005")) {
+			printf("This is gonna take a long time ei, Ctrl + C as soon as you get bored\n");
+			int result = problem_005();
+			printf("\nLowest number that is evenly divisible by 1 to 20: %d\n", result);
+			return 0;
+		}
+
+		if ( 0 == strcmp(argv[1], "problem_002")) {
+			int result = problem_006();
+			printf("Difference of the sum of the squares of the first 100 natural numbers and the squared sum of the first 100 natural numbers: %d\n", result);
+			return 0;
+		}
+
 		printf("%s\n", "The argument you gave, sadly I don't know what to do with it");
 		return 1;
 	}
 
 	if (argc < 2) {
-		printf("%s\n", "Expected at one argument");
+		printf("%s\n", "Expected one argument");
 		return 1;
 	}
 
