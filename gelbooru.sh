920ca1c254dafe0cdc71e0ef822f80c3727924b5
croisen <absolutenico32@gmail.com>
2022-08-05 21:02:38 +0800

    Deleted gelbooru py and added a page randomizer if you search with tags

--- gelbooru.sh
+++ gelbooru.sh
@@ -25,13 +25,22 @@ done
 if [[ -n "$tags" ]]; then
 	tags=$(echo $tags | sed -E 's/^\+//')
 	url="https://gelbooru.com/index.php?page=post&s=list&tags=$tags"
+	
 	echo "Currently trying to connect to Gelbooru"
-	url=($(curl -s $url | grep -soP 'https://gelbooru\.com/index\.php\?page=post&amp;s=view&amp;id=[\d\w;&=]+'))
-
-	if [[ -z $url ]]; then
+	last_page="<a href=\"\?page=post&amp;s=list&amp;tags=$tags&amp;pid=\d+"
+	page_num=$(curl -s $url | grep -soP "$last_page" | tail -n1 | grep -soP '\d+$')
+	
+	if [[ -z $page_num ]]; then
 		echo "No image found within search tags"
 		exit 1
 	fi
+
+	last_page=$(($page_num / 42))
+	random_page=$((($RANDOM % $last_page) * 42))
+
+	echo "Picking a random image"
+	url=($(curl -s "$url&pid=$random_page" | grep -soP 'https://gelbooru\.com/index\.php\?page=post&amp;s=view&amp;id=[\d\w;&=]+'))
+
 	
 	echo "Getting image source"
 	url=$(echo ${url[$RANDOM % ${#url[@]}]} | sed -E 's/amp;//g')
