c5e453e35923e00f01f042d53f6c9865bd81d4c2
croisen <absolutenico32@gmail.com>
2023-03-08 16:58:55 +0800

    printf was slowing it down somehow

--- project_euler/problem_005.c
+++ project_euler/problem_005.c
@@ -13,15 +13,13 @@ int main() {
     int continue_loop = 0;
     int divisors = 20;
     int x = divisors;
+    printf("Currently testing for a number that is evenly divisible by 1 through 20\n");
 
     while (continue_loop == 0) {
-        printf("\rCurrently testing: %9d", x);
-
         if (is_divisible_by_one_through_n(x, divisors) == 0) {
-            printf("\nLowest number that is evenly divisible by 1 through 20 is %d\n", x);
+            printf("Lowest number that is evenly divisible by 1 through 20 is %d\n", x);
             continue_loop = 1;
         }
-
         x = x + divisors;
     }
 
