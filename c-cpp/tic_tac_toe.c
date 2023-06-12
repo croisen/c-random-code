18c1cedd0c1b71f6084398c8363b583142bae1c0
croisen <absolutenico32@gmail.com>
2023-06-12 16:12:27 +0800

    Color coded tic tac toe

--- c-cpp/tic_tac_toe.c
+++ c-cpp/tic_tac_toe.c
@@ -6,43 +6,49 @@
 #define rows 3
 
 void print_board(char (*board)[cols]) {
-    printf("-------------------------\n");
+    (void) printf("-------------------------\n");
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
-            printf("|%4c   ", board[row][col]);
+            if (atoi(&board[row][col])) {
+                (void) printf("|%4c   ", board[row][col]);
+            } else if (board[row][col] == 'X') {
+                (void) printf("|   \033[1;31m%c\033[1;0m   ", board[row][col]);
+            } else {
+                (void) printf("|   \033[1;32m%c\033[1;0m   ", board[row][col]);
+            }
         }
-        printf("|\n-------------------------\n");
+        (void) printf("|\n-------------------------\n");
     }
 }
 
 int win_check(char (*board)[cols]) {
     for (int i = 0; i < rows; i++) {
         if (board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X') {
-            printf("Player 1 wins!           \n");
+            (void) printf("Player 1 wins!           \n");
             return 1;
         } else if (board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O') {
-            printf("Player 2 wins!           \n");
+            (void) printf("Player 2 wins!           \n");
             return 1;
         } else if (board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X') {
-            printf("Player 1 wins!           \n");
+            (void) printf("Player 1 wins!           \n");
             return 1;
         } else if (board[0][i] == 'O' && board[1][i] == 'O' && board[2][i] == 'O') {
-            printf("Player 2 wins!           \n");
+            (void) printf("Player 2 wins!           \n");
             return 1;
         }
     }
 
     if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
-        printf("Player 1 wins!           \n");
+        (void) printf("Player 1 wins!           \n");
         return 1;
     } else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
-        printf("Player 2 wins!           \n");
+        (void) printf("Player 2 wins!           \n");
         return 1;
     } else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
-        printf("Player 1 wins!           \n");
+        (void) printf("Player 1 wins!           \n");
         return 1;
     } else if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
-        printf("Player 2 wins!           \n");
+        (void) printf("Player 2 wins!           \n");
         return 1;
     }
     return 0;
@@ -58,10 +64,10 @@ int main() {
     };
 
     while (player_turn < 9) {
-        print_board(board);
-        printf("%s %d%-30s%s", "Player", (player_turn % 2) + 1, "'s turn:  ", "\033[20D");
+        (void) print_board(board);
+        (void) printf("%s %d%-30s%s", "Player", (player_turn % 2) + 1, "'s turn:  ", "\033[20D");
 
-        if (fgets(input_char, 64, stdin) != NULL) {
+        if (fgets(input_char, 16, stdin) != NULL) {
             input_int = atoi(input_char);
         } else {
             input_int = 10;
@@ -71,7 +77,7 @@ int main() {
             row = (input_int - 1) / 3;
             col = (input_int - 1) % 3;
         } else {
-            printf("\033[8A\rYour number is out of range bud!\n");
+            (void) printf("\033[8A\rYour number is out of range bud!\n");
             continue;
         }
 
@@ -83,22 +89,22 @@ int main() {
             }
 
             player_turn++;
-            printf("\033[8A\r");
+            (void) printf("\033[8A\r");
         } else if (board[row][col] == 'X' || board[row][col] == 'O') {
-            printf("\033[8A\rThat space is already occupied.\n");
+            (void) printf("\033[8A\rThat space is already occupied.\n");
             continue;
         } else {
-            printf("\033[8A\rYour number is out of range bud!\n");
+            (void) printf("\033[8A\rYour number is out of range bud!\n");
             continue;
         }
 
-        if (win_check(board) == 1) {
-            print_board(board);
+        if ((int) win_check(board) == 1) {
+            (void) print_board(board);
             return 0;
         }
     }
 
-    print_board(board);
-    printf("%-38s\n", "It's a tie!");
+    (void) print_board(board);
+    (void) printf("%-38s\n", "It's a tie!");
     return 0;
 }
