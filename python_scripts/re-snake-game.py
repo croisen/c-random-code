6e0c71fdc7cb68a6c14efe7ca9a61fe8da35c6c4
croisen <absolutenico32@gmail.com>
2023-06-12 16:14:59 +0800

    I'mma just hardcode the window size

--- python_scripts/re-snake-game.py
+++ python_scripts/re-snake-game.py
@@ -3,23 +3,24 @@
 
 import pygame, random, sys, time
 
-screen_range: tuple = (800, 400)
-game_screen: tuple = ((screen_range[0] * 6) // 8, (screen_range[1] * 7) // 8)
-distance_from_border: tuple = ((screen_range[0] * 2) // 36, screen_range[1] // 16)
+screen_range: tuple[int, int] = (800, 400)
+game_screen: tuple[int, int] = (600, 300)
+border_dist: tuple[int, int] = (50, 50)
 
 class Snake:
     def __init__(self):
-        self.lim_x, self.lim_y = game_screen
+        self.lim_x: int = game_screen[0] + border_dist[0] - 10
+        self.lim_y: int = game_screen[1] + border_dist[1] - 10
         self.current_dir: str = "NONE"
-        self.head_x_pos: int = random.randint(0, self.lim_x + distance_from_border[0]) // 10
-        self.head_y_pos: int = random.randint(0, self.lim_y + distance_from_border[1]) // 10
-        self.image = pygame.Surface((8, 8))
+        self.head_x_pos: int = random.randint(border_dist[0] + 10, self.lim_x)
+        self.head_y_pos: int = random.randint(border_dist[1] + 10, self.lim_y)
+        self.image: pygame.Surface = pygame.Surface((8, 8))
         self.image.fill('White')
         self.length: int = 0
         self.speed: int = 10
-        self.tail: list(tuple, ...) = []
+        self.tail: list[tuple[int, int], ...] = []
 
-    def check_and_set_dir(self, new_dir: str):
+    def check_and_set_dir(self, new_dir: str) -> None:
         if self.current_dir == "UP" and new_dir == "DOWN":
             pass
         elif self.current_dir == "LEFT" and new_dir == "RIGHT":
@@ -31,19 +32,22 @@ class Snake:
         else:
             self.current_dir = new_dir
 
-    def grow_tail(self):
+    def grow_tail(self) -> None:
         self.tail.append((self.head_x_pos, self.head_y_pos))
 
-    def cut_tail(self):
+    def cut_tail(self) -> None:
         if len(self.tail) > self.length:
             self.tail.pop()
 
+    def oob_check(self) -> bool:
+        pass
+
 class Apple:
     def __init__(self):
-        self.x_pos: int = random.randint(distance_from_border[0],
-                                         game_screen[0] + distance_from_border[0]) // 10
-        self.y_pos: int = random.randint(distance_from_border[1],
-                                         game_screen[1] + distance_from_border[1]) // 10
+        self.x_pos: int = random.randint(border_dist[0],
+                                         game_screen[0] + border_dist[0]) // 10
+        self.y_pos: int = random.randint(border_dist[1],
+                                         game_screen[1] + border_dist[1]) // 10
         self.image = pygame.Surface((8, 8))
         self.image.fill('Red')
 
@@ -56,7 +60,7 @@ def game():
     window.fill('Black')
     pygame.display.set_caption("Snake - Croisen", "Snek")
     black_screen = pygame.Surface(screen_range)
-    game_rect = pygame.Rect(distance_from_border[0] - 4, distance_from_border[1] - 4, game_screen[0] + 8, game_screen[1] + 8)
+    game_rect = pygame.Rect(border_dist[0] - 4, border_dist[1] - 4, game_screen[0] + 8, game_screen[1] + 8)
     pygame.draw.rect(black_screen, "White", game_rect, 4)
 
     clock = pygame.time.Clock()
