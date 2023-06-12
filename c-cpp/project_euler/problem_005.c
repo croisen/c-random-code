393e8e9829c85c68fc53ee782a8933feb07448de
croisen <absolutenico32@gmail.com>
2023-06-12 16:14:00 +0800

    Changed type to long int just in case

--- c-cpp/project_euler/problem_005.c
+++ c-cpp/project_euler/problem_005.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int is_divisible_by_one_through_n(int number, int divisors) {
+int is_divisible_by_one_through_n(long number, int divisors) {
     for (int i = 1; i <= divisors; i++) {
         if (number % i != 0) {
             return 1;
@@ -12,15 +12,15 @@ int is_divisible_by_one_through_n(int number, int divisors) {
 int main() {
     int continue_loop = 0;
     int divisors = 20;
-    int x = divisors;
+    long x = divisors;
     printf("Currently testing for a number that is evenly divisible by 1 through 20\n");
 
     while (continue_loop == 0) {
         if (is_divisible_by_one_through_n(x, divisors) == 0) {
-            printf("Lowest number that is evenly divisible by 1 through 20 is %d\n", x);
+            printf("Lowest number that is evenly divisible by 1 to %d is %ld\n", divisors, x);
             continue_loop = 1;
         }
-        x = x + divisors;
+        x = x + 1;
     }
 
     return 0;
