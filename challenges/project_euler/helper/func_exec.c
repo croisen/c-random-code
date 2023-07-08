cbff2fbbebcdcc91bbcc39c73d6cad30b4fc0359
croisen <absolutenico32@gmail.com>
2023-07-08 12:39:52 +0800

    Currently done with problem 1 to 12

--- challenges/project_euler/helper/func_exec.c
+++ challenges/project_euler/helper/func_exec.c
@@ -3,6 +3,7 @@
 #include "math_funcs.h"
 #include "problem_001_005.h"
 #include "problem_006_010.h"
+#include "problem_011_015.h"
 #include "func_exec.h"
 
 
@@ -13,9 +14,9 @@ void get_function(int problem_num, bool verbose) {
     f(1); f(2); f(3); f(4); f(5);
     f(6); f(7); f(8); f(9); f(10);
 
-    /*f(11); f(12); f(13); f(14); f(15);*/
+    f(11); f(12); f(13); /* f(14); f(15);*/
 
-    if ((problem_num > 10) | (problem_num == 0)) {
+    if ((problem_num > 15) | (problem_num == 0)) {
         not_implemented(problem_num);
         return;
     }
---
+++ challenges/project_euler/helper/problem_011_015.h
@@ -0,0 +1,10 @@
+#ifndef CROI_EULER_PRB_011_015_H
+#define CROI_EULER_PRB_011_015_H
+
+void problem_11(bool verbose);
+void problem_12(bool verbose);
+void problem_13(bool verbose);
+void problem_14(bool verbose);
+void problem_15(bool verbose);
+
+#endif
