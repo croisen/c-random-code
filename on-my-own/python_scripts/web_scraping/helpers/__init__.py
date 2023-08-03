5ed46b81cc26152887721c5f2a50c017fd21ee6e
croisen <absolutenico32@gmail.com>
2023-08-03 17:23:09 +0800

    Regrouping files again because I can't make up my mind

--- on-my-own/python_scripts/web_scraping/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [ "selenium_helper" ]
-__author__ = "croisen"
-__copyright__ = "Copyright 2023-present croisen"
-__license__ = "WTFPL"
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
-__title__ = "croi_web_helper"
-__version__ = "0.0.3"
